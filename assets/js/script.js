3adb85165b9a9f23d3fe24f5929785e37e6e40a8
Lucie Albane <curti.lucie@protonmail.com>
2024-09-23 14:29:07 +0200

    stopPropagation sur la modale de contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     const popUp = document.querySelector('.pop-up-overlay');
     const contact = document.querySelector('.contact');
-    const contactPhoto = document.querySelector('.single-contact-btn')
+    const contactPhoto = document.querySelector('.single-contact-btn');
     const referenceInput = document.querySelector('.ref-photo');
+    const popUpContent = document.querySelector('.pop-up');
 
     function closePopUp () {
-	    popUp.style.left = '-100%';
+        popUp.style.left = '-100%';
         popUp.style.opacity = '0';
         referenceInput.value = '';
     }
+
     function openPopUp() {
         popUp.style.left = '0';
         popUp.style.opacity = '1';
     }
+
     function openPopUpPhoto(reference) {
         popUp.style.left = '0';
         popUp.style.opacity = '1';
@@ -21,10 +24,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     contact.addEventListener('click', openPopUp);
 
-    popUp.addEventListener('click', closePopUp);
+    popUp.addEventListener('click', function(event) {
+        if (event.target === popUp) {
+            closePopUp();
+        }
+    });
+
+    popUpContent.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
 
     contactPhoto.addEventListener('click', function () {
         const reference = contactPhoto.getAttribute('data-reference');
         openPopUpPhoto(reference);
-    });    
+    });
 });
