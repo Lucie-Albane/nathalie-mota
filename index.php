47282eeb8dccd1fd46a6763d5a88abd22be364fc
Lucie Albane <curti.lucie@protonmail.com>
2024-09-24 14:01:18 +0200

    Hero header ajouté

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php get_header(); ?>
+<div class="hero-header d-flex justify-center align-center">
+    <h1 class="hero-title">PHOTOGRAPHE EVENT</h1>
+    <?php $args = array (
+		'post_type' => 'photo',
+        'posts_per_page' => 1,
+		'orderby' => 'rand'
+	);
+	$random_photo = new WP_Query($args);
+
+    if($random_photo->have_posts()){
+        while ($random_photo->have_posts()) {
+            $random_photo->the_post();
+            echo the_content();
+        }
+    } ?>
+</div>
 
 <?php get_footer(); ?>
